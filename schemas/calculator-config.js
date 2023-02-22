c8bf953cf555f5f8ae3460ba0586edf26115c1c1
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-02-22 11:02:38 -0500

    Add check another conviction button text and link to calculator config schema

--- schemas/calculator-config.js
+++ schemas/calculator-config.js
@@ -36,6 +36,17 @@ export default {
       title: 'Feedback Button Link',
     },
     {
+      name: 'checkAnotherConvictionButtonText',
+      type: 'string',
+      title: 'Check Another Conviction Button Text',
+      description: 'The label for the button that links back to the beginning of the calculator.',
+    },
+    {
+      name: 'checkAnotherConvictionButtonLink',
+      type: 'url',
+      title: 'Check Another Conviction Button Link',
+    },
+    {
       name: 'legalDisclaimer',
       type: 'text',
       title: 'Legal Disclaimer',
