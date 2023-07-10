a0d1b8d08720e10aef36435a39f6b7607ea91aa6
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-07-10 13:42:39 -0400

    Lint project

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '5af8r9p3',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
