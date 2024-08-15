70eaf43dfcba6bae9afdf5d4cfcefe18492ef222
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-08-15 16:41:17 -0400

    add block content styles for I'm not sure content block

--- schemas/calculator-config.js
+++ schemas/calculator-config.js
@@ -26,13 +26,32 @@ export default {
         {
           name: 'header',
           type: 'string',
+          styles: [{title: 'Normal', value: 'normal'}],
           title: 'Header',
         },
         {
           name: 'content',
           type: 'array',
           title: 'Content',
-          of: [{type: 'block'}],
+          of: [ 
+            {
+              type: 'block',
+              styles: [
+                {title: 'H1', value: 'h1'},
+                {title: 'H2', value: 'h2'},
+                {title: 'H3', value: 'h3'},
+                {title: 'H4', value: 'h4'},
+                {title: 'H5', value: 'h5'},
+                {title: 'H6', value: 'h6'},
+                {title: 'Heading Calc', value: 'headingCalculator'},
+                {title: 'Body 1', value: 'body1'},
+                {title: 'Body 2', value: 'body2'},
+                {title: 'Caption', value: 'caption'},
+                {title: 'Normal', value: 'normal'},
+                {title: 'Quote', value: 'blockquote'},
+              ],
+            } 
+          ],
         },
         {
           name: 'image',
